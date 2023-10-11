ba2deb80c8ecb0a7dff6826208795a9fb33dfa3b
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-10-11 16:11:26 -0600

    Making sure the tinyApp works on localhost

--- express_server.js
+++ express_server.js
@@ -2,29 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs"); // Set EJS as the view engine
+app.set("view engine", "ejs");
 
-// Define your urlDatabase
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// Route to render the URLs index page
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-// Route to render a single URL's details
 app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  const templateVars = { id, longURL };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
-
-// Other routes...
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
