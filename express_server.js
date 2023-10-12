65448fb27fa10fe980a768e14d0bbd0ad19e3d24
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-10-12 17:16:40 -0600

    Adding login to express_server.js

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cookieParser = require("cookie-parser"); // Import the cookie-parser middleware
 const app = express();
 const PORT = 8080;
+
+app.use(cookieParser()); // Use the cookie-parser middleware
 
 app.set("view engine", "ejs");
 
@@ -14,17 +17,24 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies.username }; // Pass the username cookie to the templateVars
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies.username }; // Pass the username cookie to the templateVars
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies.username }; // Pass the username cookie to the templateVars
+  res.render("urls_new", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  res.cookie("username", username); // Set the username cookie
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
