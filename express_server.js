b0e1e7d68916ee3a74530e6d5cea46bc93894ba5
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-10-17 10:07:28 -0600

    Renaming files

--- express_server.js
+++ express_server.js
@@ -5,34 +5,14 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser()); // Cookie parsing middleware
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  "9sm5xK": "http://www.google.com"
 };
 
-const users = {};
-
-function generateRandomString() {
-  // Function to generate a random string for user IDs
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
-
-// Helper function to get a user by email
-const getUserByEmail = (email) => {
-  for (const userId in users) {
-    if (users[userId].email === email) {
-      return users[userId];
-    }
-  }
-  return null;
-};
+const users = {}; // Initialize an empty users object to store user data
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -41,7 +21,7 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    user: users[req.cookies.user_id],
+    user: users[req.cookies.user_id] // Pass the user object to the template
   };
   res.render("urls_index", templateVars);
 });
@@ -50,28 +30,33 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    user: users[req.cookies.user_id],
+    user: users[req.cookies.user_id] // Pass the user object to the template
   };
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    user: users[req.cookies.user_id],
+    user: users[req.cookies.user_id] // Pass the user object to the template
   };
   res.render("urls_new", templateVars);
 });
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
-  for (const userId in users) {
-    if (users[userId].email === email && users[userId].password === password) {
-      res.cookie("user_id", userId);
-      res.redirect("/urls");
-      return;
+  if (email && password) {
+    // Search for a user with the provided email
+    for (const user in users) {
+      if (users[user].email === email) {
+        if (users[user].password === password) {
+          res.cookie("user_id", user);
+          res.redirect("/urls");
+          return;
+        }
+      }
     }
   }
-  res.status(403).send("Invalid email or password");
+  res.status(400).send("Invalid email or password");
 });
 
 app.post("/logout", (req, res) => {
@@ -81,37 +66,47 @@ app.post("/logout", (req, res) => {
 
 app.get("/register", (req, res) => {
   const templateVars = {
-    user: users[req.cookies.user_id],
+    user: users[req.cookies.user_id]
   };
   res.render("register", templateVars);
 });
 
 app.post("/register", (req, res) => {
   const { email, password } = req.body;
+  if (email && password) {
+    // Check if the email already exists
+    for (const user in users) {
+      if (users[user].email === email) {
+        res.status(400).send("Email already exists");
+        return;
+      }
+    }
 
-  // Check for empty email or password
-  if (!email || !password) {
-    res.status(400).send("Email and password cannot be empty.");
-    return;
+    const userId = generateRandomString();
+    users[userId] = {
+      id: userId,
+      email,
+      password
+    };
+
+    res.cookie("user_id", userId);
+    res.redirect("/urls");
+  } else {
+    res.status(400).send("Invalid email or password");
   }
-
-  // Check if the email already exists
-  if (getUserByEmail(email)) {
-    res.status(400).send("Email already exists.");
-    return;
-  }
-
-  const userId = generateRandomString();
-  users[userId] = {
-    id: userId,
-    email,
-    password,
-  };
-
-  res.cookie("user_id", userId);
-  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// Function to generate a random string (used for generating user IDs)
+function generateRandomString() {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
